5a353d38d8b355ac5b0d33e8121ebd2621b2eb7b
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-08-06 18:53:36 -0700

    refactor: refactor weights

--- src/constants/weights.ts
+++ src/constants/weights.ts
@@ -1,10 +1,10 @@
 import { Signal } from '../types';
 
 export const WEIGHTS: Record<Signal, number> = {
-  [Signal.AMAZON]: 0.22, // 22.00%
-  [Signal.CENSUS]: 0.11, // 11.00%
-  [Signal.BROADBAND]: 0.22, // 22.00%
-  [Signal.WALMART]: 0.11, // 11.00%
-  [Signal.BLS_PHYSICAL]: 0.0, // 0.00% (removed)
-  [Signal.BLS_ECOMMERCE]: 0.34, // 34.00%
+  [Signal.AMAZON]: 0.25,
+  [Signal.CENSUS]: 0.075,
+  [Signal.BROADBAND]: 0.2,
+  [Signal.WALMART]: 0.075,
+  [Signal.BLS_PHYSICAL]: 0.1,
+  [Signal.BLS_ECOMMERCE]: 0.3,
 };
